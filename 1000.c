e635dd2de234a2db691f108d42f7f23965eb0132
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-04-29 23:10:00 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Eduardo Nunes de Abreu Júnior
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 07/04/2026
+Objetivo    : Exibir mensagem "Hello World!" na tela usando printf.
+Aprendizado : Estrutura básica de programa C: #include, main(), return 0; Uso da função printf() para saída formatada; Sequência de escape \n para quebra de linha; Compilação e execução de programa C simples
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
