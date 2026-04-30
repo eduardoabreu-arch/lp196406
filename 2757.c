efb690b47b36a129471327c112d4685d891cb1e7
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-04-29 23:13:47 -0300

    Update 2757.c

--- 2757.c
+++ 2757.c
@@ -1,23 +1,28 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Eduardo Nunes de Abreu Júnior
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2757
+Data        : 09/04/2026
+Objetivo    : Ler 3 inteiros A, B, C e exibi-los com 4 formatos diferentes de printf.
+Aprendizado : %d: formato normal (sem preenchimento); %10d: largura mínima 10, alinhado à direita com espaços; Múltiplas leituras scanf individuais
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
     int A, B, C;
 
-    // Leitura
     scanf("%d", &A);
     scanf("%d", &B);
     scanf("%d", &C);
 
-    // 1 - formato normal
     printf("A = %d, B = %d, C = %d\n", A, B, C);
 
-    // 2 - largura 10, alinhado à direita
     printf("A = %10d, B = %10d, C = %10d\n", A, B, C);
 
-    // 3 - largura 10, preenchido com zeros
     printf("A = %010d, B = %010d, C = %010d\n", A, B, C);
 
-    // 4 - largura 10, alinhado à esquerda
     printf("A = %-10d, B = %-10d, C = %-10d\n", A, B, C);
 
     return 0;
