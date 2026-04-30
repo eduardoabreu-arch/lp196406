8a877e9f35b7b842da7e2f1765ca75140d471f7c
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-04-29 23:14:09 -0300

    Update 1013.c

--- 1013.c
+++ 1013.c
@@ -2,7 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Eduardo Nunes de Abreu Júnior
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1014
+Problema    : https://judge.beecrowd.com/pt/problems/view/1013
 Data        : 07/04/2026
 Objetivo    : Ler 3 valores inteiros e determinar qual é o maior entre eles.
 Aprendizado : Uso da função abs() da stdlib.h para valor absoluto; Leitura de 3 inteiros com scanf e saída formatada com printf
