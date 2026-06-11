0352fbc7d5e1658bcb6660dcbab2ea0e9f848285
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-06-11 00:53:33 -0300

    Create 1017.c

---
+++ 1017.c
@@ -0,0 +1,42 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Eduardo Nunes de Abreu Júnior
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : DD/MM/2026
+Objetivo    : Ler o tempo gasto em uma viagem e a velocidade média do
+              veículo, calcular a quantidade de combustível necessária
+              sabendo que o automóvel faz 12 km por litro.
+Aprendizado : Criação e utilização de funções, passagem de parâmetros,
+              cálculo de distância a partir da velocidade e do tempo,
+              manipulação de números reais e decomposição de problemas em
+              módulos reutilizáveis.
+Pergunta de segunda ordem: Quais são as vantagens de dividir um programa
+                            em funções e como isso contribui para a
+                            reutilização, manutenção e organização do código?
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+float calcularLitros(int tempo, int velocidade)
+{
+    int distancia;
+
+    distancia = velocidade * tempo;
+
+    return distancia / 12.0f;
+}
+
+int main()
+{
+    int tempo, velocidade;
+    float litros;
+
+    scanf("%d %d", &tempo, &velocidade);
+
+    litros = calcularLitros(tempo, velocidade);
+
+    printf("%.3f\n", litros);
+
+    return 0;
+}
