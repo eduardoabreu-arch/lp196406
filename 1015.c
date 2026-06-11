977637303d4e683ef4ae44bc5a19babdccb8582d
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-06-11 13:51:27 -0300

    Update 1015.c

--- 1015.c
+++ 1015.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Eduardo Nunes de Abreu Júnior
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1015
-Data        : DD/MM/2026
+Data        : 14/05/2026
 Objetivo    : Ler as coordenadas de dois pontos no plano cartesiano e
               calcular a distância entre eles utilizando a fórmula da
               distância euclidiana.
