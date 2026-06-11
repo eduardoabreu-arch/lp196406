8e43cf7d0eca9f796687de44a880569f2672cfcf
Eduardo Júnior <eduardo.abreu@unifesp.br>
2026-06-11 13:49:58 -0300

    Update 1534a.c

--- 1534a.c
+++ 1534a.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Eduardo Nunes de Abreu Júnior
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1534
-Data        : DD/MM/2026
+Data        : 12/05/2026
 Objetivo    : Ler um valor N e construir uma matriz quadrada N×N,
               preenchendo a diagonal principal com 1, a diagonal secundária
               com 2 e os demais elementos com 3.
